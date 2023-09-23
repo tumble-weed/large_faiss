b1f0061e53a86a3921fabd108edcabfbba61e59e
Aniket Singh <aniketsinghresearch@gmail.com>
2023-09-23 15:15:56 +0000

    build_index

--- build_index.py
+++ build_index.py
@@ -1,9 +1,23 @@
 from autofaiss import build_index
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--embeddings-folder',default='tiny-imagenet-200')
+parser.add_argument('--index-folder',default='index')
+parser.add_argument('--split',default='val')
+parser.add_argument('--batch-size',default=1000,type=int)
+args = parser.parse_args()
 
+embeddings_folder = args.embeddings_folder
+index_folder = args.index_folder
+split = args.split
+batch_size = args.batch_size
+# extract_patches_for_split(imagenet_folder,split,'embeddings',batch_size = batch_size)
+
+# split = 'val'
 build_index(
-embeddings="embeddings/train",
-index_path="knn.index",
-index_infos_path="infos.json",
-max_index_memory_usage="4G",
-current_memory_available="5G",
+embeddings=f"embeddings_folder/{split}",
+index_path=f"{index_folder}/{split}.index",
+index_infos_path="{index_folder}/{split}_infos.json",
+max_index_memory_usage="8G",
+current_memory_available="10G",
 )

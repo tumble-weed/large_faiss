b3adc03f1b00c1b2a1dfd71c6000f8bc20ee8f96
Aniket Singh <aniketsinghresearch@gmail.com>
2023-09-24 06:53:56 +0000

    get_cifar

---
+++ get_cifar.sh
@@ -0,0 +1,4 @@
+# wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
+wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# unzip tiny-imagenet-200.zip
+tar -xvzf cifar-10-python.tar.gz

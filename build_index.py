67f5782882f0decc7951acd7136c3789bcc3845c
Aniket Singh <aniketsinghresearch@gmail.com>
2023-09-26 11:59:49 +0000

    added my index

--- build_index.py
+++ build_index.py
@@ -1,6 +1,9 @@
 from autofaiss import build_index
 import argparse
 import os
+import time
+import json
+import faiss
 # def create_imagenet_index():
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument('--embeddings-folder',default='embeddings/tiny-imagenet-200')
@@ -49,10 +52,10 @@ def main():
     index_folder = args.index_folder
     split = args.split
     current_memory_available = args.current_memory_available
-    create_index(dataset,split,
+    create_index_autofaiss(dataset,split,
 current_memory_available,embeddings_folder,index_folder)
 
-def create_index(dataset,split,size,
+def create_index_autofaiss(dataset,split,size,
 current_memory_available,embeddings_folder,index_folder):
     '''
     current_memory_available is the parameter controlling the maximum amount of
@@ -64,12 +67,65 @@ current_memory_available,embeddings_folder,index_folder):
 
     '''
     # import ipdb;ipdb.set_trace()
-    build_index(
-    embeddings=embeddings_folder,
-    index_path=f"{index_folder}/{dataset}_{split}_{size}.index",
-    index_infos_path=f"{index_folder}/{dataset}_{split}_{size}_infos.json",
-    max_index_memory_usage="10G",
-    current_memory_available=f"{current_memory_available}G",
-    )
+    index_path = f"{index_folder}/{dataset}_{split}_{size}.index"
+    index_infos_path = f"{index_folder}/{dataset}_{split}_{size}_infos.json"
+    if True:
+        build_index(
+        embeddings=embeddings_folder,
+        index_path=index_path,
+        index_infos_path=index_infos_path,
+        max_index_memory_usage="10G",
+        current_memory_available=f"{current_memory_available}G",
+        )
+    return index_path,index_infos_path
+
+def create_index_mine(dataset,split,size,
+# current_memory_available,
+# patch_dim,
+embeddings_fname,
+# npatches,
+index_folder,
+nlist = 2048,
+m = 7,
+nbits = 8,
+):
+    '''
+    current_memory_available is the parameter controlling the maximum amount of
+    ram to use, it's that one you need to put at 16GB if you want to use only
+    16GB while creating the index
+
+    max_index_memory_usage controls the size of the built index, which is
+    different.
+
+    '''
+    # import ipdb;ipdb.set_trace()
+    index_path = f"{index_folder}/{dataset}_{split}_{size}.index"
+    index_infos_path = f"{index_folder}/{dataset}_{split}_{size}_infos.json"
+    
+    if True:
+
+        with open(embeddings_fname.replace('.memmap','.info'),'r') as f:
+            info = json.load(f)
+        data = np.memmap(embeddings_fname, dtype='float32', mode='r',shape=(info['total_len'],info['patch_dim']))
+        quantizer = faiss.IndexFlatL2(info['patch_dim']) 
+        
+        index = faiss.IndexIVFPQ(quantizer, info['patch_dim'], nlist, m, nbits)  # 8 
+        
+        tic = time.time()
+        print("Training the index...")
+        index.train(data)         
+        toc = time.time()
+        print('time taken to create',toc-tic)
+        print("Adding data")
+        tic = time.time()
+        index.add(data)
+        toc = time.time()
+        print('time taken to add',toc-tic)
+        faiss.write_index(index, index_path)
+
+
+    return index_path,index_infos_path
+
+
 if __name__ == '__main__':
     main()
